cc0b25ad1e23ddb3e9fefb37493fd2dffcf84f63
VektorTech <kennymax18@gmail.com>
2022-12-23 05:46:23 -0500

    prevent session and listen during tests

--- server/src/index.js
+++ server/src/index.js
@@ -7,27 +7,41 @@ import cookieParser from "cookie-parser";
 import MongoStore from "connect-mongo";
 import passport from "passport";
 import authRouter from "./routes/auth.js";
+import quizRouter from "./routes/quiz.js";
 
 const app = express();
 const PORT = 3001;
 
-const data = await mongoose.connect(process.env.MONGODB_URL);
-console.log(`Mongo DB is connected to server: ${data.connection.host}`);
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then((data) =>
+    console.log(`Mongo DB is connected to server: ${data.connection.host}`)
+  );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  session({
-    secret: "some-secret",
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URL }),
-  })
-);
-app.use(passport.authenticate("session"));
-app.use("/api", authRouter);
 
-app.listen(PORT, () => {
-  console.log(`Listening on PORT: ${PORT}`);
-});
+if (process.env.NODE_ENV != "test") {
+  app.use(
+    session({
+      secret: "some-secret",
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({ mongoUrl: process.env.MONGODB_URL }),
+    })
+  );
+  app.use(passport.authenticate("session"));
+}
+
+app.use("/api", authRouter);
+app.use("/api", quizRouter);
+
+if (process.env.NODE_ENV != "test") {
+  app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`);
+  });
+}
+
+export default app;

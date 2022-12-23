9d77de56004f895200ad30dddc6f417f3eb9c7c4
VektorTech <kennymax18@gmail.com>
2022-12-23 13:07:51 -0500

    use different database for testing purposes

--- server/src/index.js
+++ server/src/index.js
@@ -9,22 +9,26 @@ import passport from "passport";
 import authRouter from "./routes/auth.js";
 import quizRouter from "./routes/quiz.js";
 
-const app = express();
 const PORT = 3001;
+const MONGODB_URL =
+  process.env.NODE_ENV == "test"
+    ? process.env.MONGODB_TEST_URL
+    : process.env.MONGODB_URL;
 
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(process.env.MONGODB_URL)
+  .connect(MONGODB_URL)
   .then((data) =>
     console.log(`Mongo DB is connected to server: ${data.connection.host}`)
   );
 
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 export const sessionStore = MongoStore.create({
-  mongoUrl: process.env.MONGODB_URL,
+  mongoUrl: MONGODB_URL,
 });
 
 app.use(

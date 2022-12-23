8cb22adecf89992afa594d8aa225f338f1c85308
VektorTech <kennymax18@gmail.com>
2022-12-23 03:54:12 -0500

    use mongodb as session store
    - add more configurations to the server

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,33 @@
-const express = require('express');
+import "./configs/env.config.js";
+
+import express from "express";
+import mongoose from "mongoose";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import MongoStore from "connect-mongo";
+import passport from "passport";
+import authRouter from "./routes/auth.js";
+
 const app = express();
-const port = 3001;
+const PORT = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const data = await mongoose.connect(process.env.MONGODB_URL);
+console.log(`Mongo DB is connected to server: ${data.connection.host}`);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "some-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URL }),
+  })
+);
+app.use(passport.authenticate("session"));
+app.use("/api", authRouter);
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`);
 });

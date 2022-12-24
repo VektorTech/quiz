df15b764c5e42b55b0e9d6d4e9d4c58b7bc977da
VektorTech <kennymax18@gmail.com>
2022-12-24 16:59:51 -0500

    remove quiz responses in teardown

--- server/test/fixtures.js
+++ server/test/fixtures.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 import { sessionStore } from "../src/index.js";
 import Quiz from "../src/models/quiz.js";
+import QuizResponse from "../src/models/quizResponse.js";
 import User from "../src/models/user.js";
 import { SessionModel } from "./testHelpers.js";
 
@@ -9,6 +10,7 @@ export async function mochaGlobalTeardown() {
   await SessionModel.deleteMany();
   await User.deleteMany();
   await Quiz.deleteMany();
+  await QuizResponse.deleteMany();
 
   await sessionStore.close();
   await mongoose.connection.close();

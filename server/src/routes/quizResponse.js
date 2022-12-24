f56e8cd7a3c99b200c3a81f02ed23dbd1537fb96
VektorTech <kennymax18@gmail.com>
2022-12-24 16:57:00 -0500

    add routes for handling quiz responses

--- server/src/routes/quizResponse.js
+++ server/src/routes/quizResponse.js
@@ -1,9 +1,32 @@
 import { Router } from "express";
+import Quiz from "../models/quiz.js";
+import QuizResponse from "../models/quizResponse.js";
 
 const quizResponseRouter = Router();
 
-quizResponseRouter.post("/", (req, res) => {
-  res.status(201).json({});
+quizResponseRouter.post("/", async (req, res) => {
+  const { quiz: quizID, answers } = req.body;
+  const quiz = await Quiz.findById(quizID);
+
+  if (quiz && quiz.status == "active") {
+    const quizResponse = new QuizResponse({
+      quiz: quiz.id,
+      answers
+    });
+    await quizResponse.save();
+
+    return res.status(201).json({ data: quizResponse });
+  }
+
+  res.status(404).send("Quiz Not Found");
+});
+
+quizResponseRouter.get("/:quizID", async (req, res) => {
+  const quiz = await Quiz.findById(req.params.quizID);
+
+  const responses = await QuizResponse.find({ quiz: quiz.id });
+
+  res.status(201).json({ data: responses });
 });
 
 export default quizResponseRouter;
--- server/test/quizResponse.test.js
+++ server/test/quizResponse.test.js
@@ -4,6 +4,7 @@ import assert from "assert";
 import app from "../src/index.js";
 import { createTestUser, QuizSamples, USER_ID } from "./testHelpers.js";
 import Quiz from "../src/models/quiz.js";
+import QuizResponse from "../src/models/quizResponse.js";
 import User from "../src/models/user.js";
 
 const api = supertest(app);
@@ -11,6 +12,7 @@ const api = supertest(app);
 beforeEach(async () => {
   encodeURIComponent(await createTestUser());
   await Quiz.deleteMany();
+  await QuizResponse.deleteMany();
   const quizzes = await Quiz.create(QuizSamples);
   await User.findByIdAndUpdate(USER_ID, {
     $set: {
@@ -30,5 +32,35 @@ describe("POST /api/responses", () => {
     });
 
     assert.strictEqual(response.statusCode, 201);
+
+    const allResponses = await QuizResponse.find();
+    assert.strictEqual(allResponses.length, 1);
+    assert.strictEqual(allResponses[0].quiz.toString(), quiz.id);
+  });
+});
+
+describe("GET /api/responses/:quizID", async () => {
+  const quiz = await Quiz.findOne({ title: QuizSamples[0].title });
+
+  before(async () => {
+    await QuizResponse.create({
+      quiz: quiz.id,
+      answers: "[{1}]"
+    });
+    await QuizResponse.create({
+      quiz: quiz.id,
+      answers: "[{2}]"
+    });
+    await QuizResponse.create({
+      quiz: quiz.id,
+      answers: "[{3}]"
+    });
+  });
+
+  it("should get all responses for a quiz", async () => {
+    const responses = await api.get(`/api/responses/${quiz.id}`);
+
+    assert.strictEqual(responses.statusCode, 200);
+    assert.strictEqual(responses.body.data.length, 3);
   });
 });

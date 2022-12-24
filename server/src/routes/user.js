94a84d6035c212066471b9816fa377680910c10f
VektorTech <kennymax18@gmail.com>
2022-12-24 18:12:02 -0500

    join quizzes & likes in user query

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -7,7 +7,10 @@ const userRouter = Router();
 
 userRouter.get("/:id", ensureLoggedIn(), async (req, res) => {
   if (req.params.id == req.user.id) {
-    const user = await User.findById(req.user.id);
+    const user = await User.findById(req.user.id).populate([
+      "quizzes",
+      "likedQuizzes",
+    ]);
     return res.json({ data: { ...user.toObject(), user_id: null } });
   }
 

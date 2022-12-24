995fe51e41a407a04e9284992669106c29545c62
VektorTech <kennymax18@gmail.com>
2022-12-24 13:13:57 -0500

    add cors to express

--- server/src/index.js
+++ server/src/index.js
@@ -7,10 +7,12 @@ import session from "express-session";
 import cookieParser from "cookie-parser";
 import MongoStore from "connect-mongo";
 import passport from "passport";
+import cors from "cors";
+
 import authRouter from "./routes/auth.js";
 import quizRouter from "./routes/quiz.js";
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const MONGODB_URL =
   process.env.NODE_ENV == "test"
     ? process.env.MONGODB_TEST_URL
@@ -21,16 +23,19 @@ mongoose
   .connect(MONGODB_URL)
   .then((data) =>
     console.log(`Mongo DB is connected to server: ${data.connection.host}`)
-  );
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+  )
+  .catch(() => console.error("Error Connecting to DB"));
 
 export const sessionStore = MongoStore.create({
   mongoUrl: MONGODB_URL,
 });
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(
   session({

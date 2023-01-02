a0c5a31d35c9efb9b20e73f572a2b00e18a7b6d9
VektorTech <kennymax18@gmail.com>
2023-01-02 11:45:30 -0500

    disable devtools during prod

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -10,6 +10,7 @@ const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(userApi.middleware, quizApi.middleware),
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 setupListeners(store.dispatch);

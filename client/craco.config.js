b5527f6a71548d22dca1a217d97688619bab875a
VektorTech <kennymax18@gmail.com>
2023-01-02 20:02:25 -0500

    enable path aliasing in jest

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,4 +1,6 @@
 const path = require(`path`);
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
 
 module.exports = {
   webpack: {
@@ -6,4 +8,12 @@ module.exports = {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  jest: {
+    configure: {
+      preset: "ts-jest",
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: "<rootDir>"
+      })
+    }
+  }
 };

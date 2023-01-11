5df0f27ff74e9ff9140eaaa069c95be80a9117dc
VektorTech <kennymax18@gmail.com>
2023-01-11 18:28:21 -0500

    load auth user quizzes into quiz slice.

--- client/src/features/quiz/quizSlice.ts
+++ client/src/features/quiz/quizSlice.ts
@@ -1,6 +1,7 @@
-import store, { RootState } from "@/app/store";
+import { RootState } from "@/app/store";
 import { CATEGORIES } from "@/libs/constants";
-import { quizApi } from "@/services/quiz";
+import baseAPI from "@/services/api";
+
 import { createEntityAdapter, createSlice, nanoid } from "@reduxjs/toolkit";
 
 export interface QuestionType {
@@ -40,10 +41,22 @@ const quizSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder.addMatcher(
-      quizApi.endpoints.addQuiz.matchFulfilled,
+      baseAPI.endpoints.addQuiz.matchFulfilled,
       (state, { payload }) => {
         const schema = JSON.parse(payload.data.surveySchema);
         quizAdapter.addOne(state, { id: nanoid(5), ...schema });
+      }
+    );
+    builder.addMatcher(
+      baseAPI.endpoints.getAuthQuizzes.matchFulfilled,
+      (state, { payload }) => {
+        quizAdapter.setAll(
+          state,
+          payload.ids.map((id) => ({
+            id,
+            ...JSON.parse(payload.entities[id]?.surveySchema ?? ""),
+          }))
+        );
       }
     );
   },
@@ -52,7 +65,7 @@ const quizSlice = createSlice({
 export const { quizAdded, quizUpdated, quizDeleted, quizzesReceived } =
   quizSlice.actions;
 
-export const { selectById: selectQuizById, selectAll } =
+export const { selectById: selectQuizById, selectAll: selectAllQuizzes } =
   quizAdapter.getSelectors<RootState>((state) => state.quizzes);
 
 export default quizSlice.reducer;

9b979b754d5a0e7fb9d810cbfca5b59b82bdbc21
VektorTech <kennymax18@gmail.com>
2023-01-15 00:22:28 -0500

    add footer to layout
    - clearfix for next button on quiz page

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import { Box, Container, Stack, Text } from "@chakra-ui/react";
+
+export default function Footer() {
+  return (
+    <Box as="footer" bgColor="gray.900" pt="3" pb="3">
+      <Container maxW="container.lg" textAlign="center">
+        <Stack color="gray.400" spacing={0}>
+          <Text fontSize="sm">
+            &copy; {new Date().getFullYear()} QuizWrld. All Rights Reserved.
+          </Text>
+          <Text fontSize="sm">Developed by Kenny Sutherland.</Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "@/components/Header";
+import { Footer } from "../Footer";
+import { Box } from "@chakra-ui/react";
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <Box as="main" mb="10">
+        <Outlet />
+      </Box>
+      <Footer />
     </>
   );
 };

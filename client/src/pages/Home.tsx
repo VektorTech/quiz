2622132577d8c6ddb2d769a66a41fba19e8de46a
VektorTech <kennymax18@gmail.com>
2023-01-16 18:15:34 -0500

    format file

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,9 +7,10 @@ import {
   Image,
   Stack,
   Text,
+  Container,
+  Grid,
+  GridItem,
 } from "@chakra-ui/react";
-
-import { Container, Grid, GridItem } from "@chakra-ui/react";
 
 import PlaceholderImage from "@/assets/quiz-img-placeholder.jpg";
 import { useGetQuizzesQuery } from "@/services/api";
@@ -20,29 +21,29 @@ export default function Home() {
   if (!data) return null;
 
   return (
-    <Container maxW="container.lg" gap={6}>
-      <Heading mt="5" mb="5" textAlign={"center"}>
-        QuizWrld
-      </Heading>
-
-      <Text>
+    <Container maxW="container.lg" gap="6">
+      <Text mt="5">
         Welcome to the ultimate quiz site! Here, you can test your knowledge and
         challenge your friends to see who comes out on top. With a wide variety
         of quizzes covering topics ranging from pop culture to history, there's
         something for everyone.
       </Text>
 
-      <Grid mt="10" templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={5}>
+      <Grid
+        mt="10"
+        templateColumns="repeat(auto-fit, minmax(220px, 1fr))"
+        gap="3"
+      >
         {data.data.map((quiz) => (
-          <GridItem key={quiz.id}>
+          <GridItem key={quiz.id} maxW="320px">
             <Link to={{ pathname: quiz.slug }}>
               <Card maxW="sm" float="left">
-                <Stack direction={"row"} justifyContent={"flex-end"}>
+                <Stack direction="row" justifyContent="flex-end">
                   <Badge colorScheme="purple">{quiz.category}</Badge>
                 </Stack>
                 <Image
                   objectFit="cover"
-                  width={"100%"}
+                  width="100%"
                   src={quiz.image ?? PlaceholderImage}
                   alt={quiz.title}
                 />

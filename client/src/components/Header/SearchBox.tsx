12c8c1775c2120df5ef9c92113e170ee0641ce33
VektorTech <kennymax18@gmail.com>
2023-01-20 23:52:13 -0500

    use Form from react-router-dom for search box
    adds search param to url on submit through router nav

--- client/src/components/Header/SearchBox.tsx
+++ client/src/components/Header/SearchBox.tsx
@@ -1,25 +1,37 @@
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { Center, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+import { Form } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const SearchForm = styled(Form)`
+  width: 100%;
+`;
 
 const SearchBox = ({ full = false }) => {
   return (
-    <InputGroup
-      marginRight={full ? 0 : "5"}
-      display="inline"
-      width={full ? "100%" : "fit-content"}
-    >
-      <InputLeftElement
-        pointerEvents="none"
-        children={<SearchIcon boxSize={6} color="gray.300" />}
-      />
-      <Input
-        htmlSize={full ? 0 : 12}
-        width={full ? "100%" : "auto"}
-        // focusBorderColor="brand.300"
-        type="search"
-        placeholder="Search Quizzes"
-      />
-    </InputGroup>
+    <SearchForm action="/browse" role="searchbox">
+      <Center>
+        <InputGroup
+          marginRight={full ? 0 : 5}
+          display="inline"
+          width={full ? "100%" : "fit-content"}
+        >
+          <InputLeftElement
+            pointerEvents="none"
+            children={<SearchIcon boxSize="6" color="gray.300" />}
+          />
+          <Input
+            htmlSize={full ? 0 : 12}
+            width={full ? "100%" : "auto"}
+            type="search"
+            aria-label="Search quizzes"
+            placeholder="Search Quizzes"
+            name="search"
+          />
+        </InputGroup>
+        <input type="submit" hidden />
+      </Center>
+    </SearchForm>
   );
 };
 

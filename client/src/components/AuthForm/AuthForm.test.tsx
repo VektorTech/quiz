895a62dfa1ac50ec8ec4b02c3b7fa0bdd1067b4d
VektorTech <kennymax18@gmail.com>
2023-01-20 23:47:13 -0500

    add modal/form for authenticating users

---
+++ client/src/components/AuthForm/AuthForm.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+
+import store from "@/app/store";
+
+import AuthForm from "./AuthForm";
+import { useAppDispatch } from "@/app/hooks";
+
+import { openModal } from "@/features/ui/uiSlice";
+
+const App = () => {
+  const dispatch = useAppDispatch();
+  dispatch(openModal("LOGIN"));
+  return <AuthForm />;
+};
+
+jest.setTimeout(20 * 1000);
+
+test("should verify AuthForm renders correctly", async () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    { wrapper: MemoryRouter }
+  );
+
+  const logInButton = await screen.findByText("Log In");
+  const googleButton = await screen.findByText("Continue with Google");
+
+  const emailInput = await screen.findByLabelText("Email");
+  const passwordInput = await screen.findByLabelText("Password");
+  await userEvent.clear(emailInput);
+  await userEvent.clear(passwordInput);
+
+  await userEvent.click(emailInput);
+  await userEvent.keyboard("email");
+  await userEvent.click(passwordInput);
+  await userEvent.keyboard("pass");
+  await userEvent.click(emailInput);
+
+  expect(await screen.findByText("Invalid email address")).toBeVisible();
+  expect(await screen.findByText("Must be at least 7 characters")).toBeVisible();
+
+  expect(logInButton).toBeVisible();
+  expect(googleButton).toBeVisible();
+});
---
+++ client/src/components/AuthForm/AuthForm.tsx
@@ -0,0 +1,162 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Center,
+  Button,
+  HStack,
+  Text,
+  Divider,
+  Stack,
+  FormControl,
+  FormLabel,
+  InputGroup,
+  InputLeftElement,
+  FormErrorMessage,
+  Input,
+} from "@chakra-ui/react";
+import { EditIcon, EmailIcon, LockIcon } from "@chakra-ui/icons";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+import GoogleIcon from "@/components/Icons/GoogleIcon";
+import { closeModal, selectModalState } from "@/features/ui/uiSlice";
+import { useAppSelector, useAppDispatch } from "@/app/hooks";
+import { useRopcLoginMutation } from "@/services/api";
+
+export default function AuthForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    mode: "onBlur",
+    defaultValues: {
+      username: "johnbrown@gmail.com",
+      password: "Password123",
+    },
+  });
+  const [ropcLogin] = useRopcLoginMutation();
+  const modalState = useAppSelector(selectModalState);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  return (
+    <Modal
+      isOpen={modalState === "LOGIN"}
+      onClose={() => dispatch(closeModal())}
+    >
+      <ModalOverlay />
+      <ModalContent maxW="18rem">
+        <ModalHeader>Login</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Center>
+            <Button
+              width="100%"
+              onClick={() => {
+                window.location.href = "//localhost:3001/api/auth/login/google";
+              }}
+              leftIcon={<GoogleIcon />}
+            >
+              Continue with Google
+            </Button>
+          </Center>
+
+          <HStack mt="3" mb="3">
+            <Divider />
+            <Text style={{ fontVariant: "small-caps" }} pl="1" pr="1">
+              or
+            </Text>
+            <Divider />
+          </HStack>
+
+          <form
+            onSubmit={handleSubmit(async (data) => {
+              try {
+                await ropcLogin(data).unwrap();
+                navigate("/me");
+              } catch (e) {}
+            })}
+          >
+            <Stack gap="1">
+              <FormControl isInvalid={!!errors.username}>
+                <FormLabel>Email</FormLabel>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<EmailIcon color="gray.300" />}
+                  />
+                  <Input
+                    type="email"
+                    {...register("username", {
+                      required: "Email is required!",
+                      pattern: {
+                        value: emailPattern,
+                        message: "Invalid email address",
+                      },
+                    })}
+                  />
+                </InputGroup>
+                <FormErrorMessage>
+                  {errors.username && errors.username.message?.toString()}
+                </FormErrorMessage>
+              </FormControl>
+
+              <FormControl isInvalid={!!errors.password}>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<LockIcon color="gray.300" />}
+                  />
+                  <Input
+                    type="password"
+                    {...register("password", {
+                      required: true,
+                      minLength: {
+                        value: 7,
+                        message: "Must be at least 7 characters",
+                      },
+                    })}
+                  />
+                </InputGroup>
+                <FormErrorMessage>
+                  {errors.password && errors.password.message?.toString()}
+                </FormErrorMessage>
+              </FormControl>
+
+              <Button
+                isLoading={isSubmitting}
+                width="100%"
+                type="submit"
+                colorScheme="brand"
+              >
+                Log In
+              </Button>
+
+              <Button
+                variant="ghost"
+                leftIcon={<EditIcon />}
+                fontSize="sm"
+                color="gray.600"
+                type="button"
+                onClick={() => {
+                  window.location.href = "//localhost:3001/api/auth/login";
+                }}
+              >
+                Create An Account
+              </Button>
+            </Stack>
+          </form>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+const emailPattern =
+  /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$/;

2f6e6f5e13065561cecb76474736c6e38cc3f8be
VektorTech <kennymax18@gmail.com>
2023-01-20 23:47:34 -0500

    add test for footer component

---
+++ client/src/components/Footer/Footer.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+
+import Footer from "./Footer";
+
+test("should verify footer renders correctly", async () => {
+  render(<Footer />);
+
+  const copyText = await screen.findByText(`© ${new Date().getFullYear()} QuizWrld. All Rights Reserved.`);
+  const devText = await screen.findByText("Developed by Kenny Sutherland.");
+
+  expect(copyText).toBeVisible();
+  expect(devText).toBeVisible();
+});

5c58de639e9dce905853bbfb1c0b9bd2ab8774c6
VektorTech <kennymax18@gmail.com>
2023-02-25 01:33:58 -0500

    add cookie config

--- server/src/index.js
+++ server/src/index.js
@@ -45,6 +45,10 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: sessionStore,
+    cookie: {
+      secure: true,
+      sameSite: "none"
+    }
   })
 );
 app.use(passport.authenticate("session"));
